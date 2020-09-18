e9be86e6fe31f1ddfaa58f5440f8cd433cedd4a6
Мария <slinkywizard@mail.ru>
2020-09-18 19:38:45 +0300

    Исправила ошибки в разметке в тегах picture

---
+++ source/js/main.js
@@ -0,0 +1,72 @@
+/*****************************МОБИЛЬНОЕ МЕНЮ**********************************/
+
+//var burgerMenu = document.querySelector(".burger-menu");
+
+var buttonBurger = document.querySelector(".page-header__mobile-menu-burger");
+var mobileMenu = document.querySelector(".site-nav");
+var closeButton = document.querySelector(".page-header__mobile-menu-close");
+var whiteLogo = document.querySelector(".logo--white");
+var blueLogo = document.querySelector(".logo--blue");
+//console.log(mobileLogo);
+
+mobileMenu.classList.add("site-nav--hide"); // спрятанное моб меню при работающем js
+buttonBurger.classList.remove("page-header__mobile-menu-burger--nojs");
+closeButton.classList.remove("page-header__mobile-menu-close--nojs");
+whiteLogo.classList.remove("logo--white--nojs");
+//console.log("text");
+
+buttonBurger.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mobileMenu.classList.toggle("site-nav--hide");
+  mobileMenu.classList.add("site-nav--show");
+  buttonBurger.classList.add("page-header__mobiel-menu-burger--hide");
+  closeButton.classList.add("page-header__mobile-menu-close--show");
+});
+
+closeButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mobileMenu.classList.toggle("site-nav--hide");
+  mobileMenu.classList.add("site-nav--show");
+  buttonBurger.classList.remove("page-header__mobile-menu-burger--hide");
+  closeButton.classList.remove("page-header__mobile-menu-close--show");
+});
+
+/*
+var burgerFlag = true;
+
+burgerMenu.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (burgerFlag) {
+    mobileMenu.classList.toggle("site-nav--hide");
+    mobileMenu.classList.add("site-nav--show");
+    buttonBurger.classList.add("burger-menu__button--hide");
+    //console.log("text");
+    console.log(burgerFlag);
+    mobileLogo.classList.remove("page-header__logo");
+    mobileLogo.classList.add("page-header__logo--hide");
+    //buttonBurger.classList.add("burger-menu__button--hide");
+    closeButton.classList.add("burger-menu__close--show");
+    burgerFlag = false;
+  } else {
+    console.log(burgerFlag);
+    mobileMenu.classList.remove("site-nav--show");
+    mobileMenu.classList.add("site-nav--hide");
+    buttonBurger.classList.remove("burger-menu__button--hide");
+    mobileLogo.classList.remove("page-header__logo--hide");
+    closeButton.classList.remove("burger-menu__close--show");
+    burgerFlag = true;
+  }
+});
+*/
+
+
+/*
+closeButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mobileMenu.classList.remove("site-nav--show");
+  mobileMenu.classList.add("site-nav--hide");
+  buttonBurger.classList.remove("burger-menu__button--hide");
+  mobileLogo.classList.remove("page-header__logo--hide");
+  closeButton.classList.add("burger-menu__close--show");
+});
+*/
